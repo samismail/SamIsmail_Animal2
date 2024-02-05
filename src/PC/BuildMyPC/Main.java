308089d94b17a10ef99cdd3a1abfeb5f9575e61d
Bryanah1 <balvarado2@ggc.edu>
2024-02-05 12:18:08 -0500

    initial

---
+++ src/PC/BuildMyPC/Main.java
@@ -0,0 +1,11 @@
+package PC.BuildMyPC;
+
+public class Main {
+
+    public static void main(String args[]) {
+        Dimension myDimension = new Dimension()
+        Case myCase = new Case("4000D AIRFLOW ATX", "Corsair", "Steel, Glass, Plastic", myDimension);
+        PC homePC = new PC();
+        //
+    }
+}
